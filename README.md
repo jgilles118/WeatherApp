2af684334098383971aae860a5edfdd6bf277c22
Jemz G <jgilles123@gmail.com>
2020-07-11 22:09:45 -0400

    Completed the app. background image not populating

--- README.md
+++ README.md
@@ -12,9 +12,14 @@ error, Tk may need to be installed.
 	* __dnf__: dnf install python3-tkinter
 	* __yum__: yum install tkinter tk-devel
 	
-You may need to install 2 more libraries 
+You may need to install 2 more libraries: 
+1) First install PIP __sudo pacman -S python-pip__ 
 	* Requests - API handler
 	* Pillow - The python imaging library
+2) Run the command: pip install reqeusts Pillow
+3) Sign-up to: OpenWeatherMap.org it's FREE!!
+
+
 
 ## Python Documentation
 https://www.tutorialspoint.com/python
--- weatherGUI.py
+++ weatherGUI.py
@@ -1,8 +1,31 @@
 #!/usr/bin/python3
 
 import tkinter as tk
+import requests
 
+#Assigning Tk() function to window
 window = tk.Tk()
+
+#Click action
+# api.openweathermap.org/data/2.5/forecast?q={city name},{state code},{country code}&appid={your api key}
+def search_weather(city):
+    try:
+        weather_key = '412242770eab22d4cc2baf26ff3436d6'
+        url = 'https://api.openweathermap.org/data/2.5/weather'
+        parameter = {'APPID':weather_key, 'q':city, 'units':'imperial'}
+        response = requests.get(url, params=parameter)
+        results = response.json()
+        name = 'City: ' + results['name']
+        skys = 'Skys: ' + results['weather'][0]['description']
+        temp = 'Currently: ' + str(results['main']['temp'])
+        accTemp = 'Feels like: ' + str(results['main']['feels_like'])
+        humd = 'Humidity: ' + str(results['main']['humidity'])
+        final = name + '\n' + skys + '\n' + temp + '\n' + accTemp + '\n' + humd
+    except:
+        final = 'No temperature in The Land of Make Believe!'
+
+    #To display in the app
+    label['text'] = final
 
 #The widget title
 window.title("Weather App")
@@ -10,7 +33,11 @@ window.title("Weather App")
 #The widget size
 winsize = tk.Canvas(window, height=700, width=800)
 winsize.pack()
-
+'''
+bg_image = tk.PhotoImage(file='LandScape2.png')
+bg_label = tk.Label(window, image=bg_image)
+bg_label.place(relheight=1,relwidth=1)
+'''
 #Frames and  colors
 frame1 = tk.Frame(window, bg='#80c1ff', bd=6)
 frame1.place(relx=0.5, rely=0.1,relheight=0.1, relwidth=0.75, anchor='n')
@@ -19,7 +46,7 @@ frame2 = tk.Frame(window, bg='#80c1fa', bd=6)
 frame2.place(rely=0.25,relx=0.5, relheight=0.6,relwidth=0.75, anchor='n') 
 
 #The search button
-search = tk.Button(frame1, text="Peek out your window!", font=30)
+search = tk.Button(frame1, text="Peek out your window!", font=30, command=lambda: search_weather(userin.get()))
 search.place(relx=0.7, relheight=1, relwidth=0.3) 
 
 #Label
@@ -29,6 +56,5 @@ label.place(relheight=1,relwidth=1)
 #Input box
 userin = tk.Entry(frame1, font=30)
 userin.place(relheight=1, relwidth=0.65)
-
 
 window.mainloop()
